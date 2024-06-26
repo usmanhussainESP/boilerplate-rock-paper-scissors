ef447ca046ff85c011beb3d1a479e6eccdaef266
Usman Hussain <usman.hussain@logitek.es>
2024-06-26 13:17:42 +0000

    se ha añadido la solución

--- RPS.py
+++ RPS.py
@@ -1,10 +1,25 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+def player(prev_play, opponent_history=[], play_order={}):
+    if not prev_play:
+        prev_play = 'R'
 
-def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
+    
+    window_size = 5
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    if len(opponent_history) < window_size:
+        return 'S' 
 
-    return guess
+    last_sequence = "".join(opponent_history[-window_size:])
+    play_order[last_sequence] = play_order.get(last_sequence, 0) + 1
+    
+    if len(opponent_history) > window_size:
+        opponent_history.pop(0)
+
+    potential_plays = [last_sequence[-4:] + next_play for next_play in 'RPS']
+    sub_order = {play: play_order.get(play, 0) for play in potential_plays}
+
+    prediction = max(sub_order, key=sub_order.get)[-1]
+
+    ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
+
+    return ideal_response[prediction]
